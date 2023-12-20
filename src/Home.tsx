84a3652156f5c2a87125227485e23f7d7f2fa413
nathan-lesny <nlesny15@gmail.com>
2023-12-19 22:41:53 -0500

    Landing page design

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,29 @@
+import Navbar from "./components/Navbar";
+import './home.css'
+
+const API_BASE: string = "localhost:3000"
+
 const Home = () => {
   return (
     <main>
-      <div>
-        <p>Yeetus my cleetus</p>
+      <div className="home-body">
+        <Navbar />
+        <div className="home-spacer" />
+        <div className="home-content">
+          <p id="home-content-title">
+            rrevif brings work to freelance developers, 
+            and employees to businesses
+          </p>
+          <p className="home-content-desc">
+            Build up your portfolio and make money
+          </p>
+          <a href={API_BASE} className="a-btn">
+            <div className="submit-button">
+              <p className="submit-text">Sign up - it's free</p>
+            </div>
+          </a>
+          <div className="home-spacer" />
+        </div>
       </div>
     </main>
   )
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import './navbar.css'
+
+const API_BASE: string = "localhost:3000";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1 className='nav-logo'>rrevif</h1>
+      <div className="nav-links">
+        <a className="no-style" href={API_BASE}><h4 className='nav-link'>Features</h4></a>
+        <a className="no-style" href={API_BASE}><h4 className='nav-link'>Listings</h4></a>
+        <a className="no-style" href={API_BASE}><h4 className='nav-link'>About</h4></a>
+        <a className="no-style" href={API_BASE}><h4 className='nav-link'>Home</h4></a>
+      </div>
+      <div className="nav-btn-lgn">
+        <a href={API_BASE} className='nav-link nav-login no-style'>Log in</a>
+        <a href={API_BASE} className="no-style">
+        <div className="nav-exclamation">
+          Get rrivef for free
+        </div>
+        </a>
+      </div>
+    </div>
+  )
+};
+
+export default Navbar;
