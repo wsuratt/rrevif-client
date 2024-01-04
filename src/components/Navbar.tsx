e9d4ff64c7844dc440983a47d0b592a76ed9f365
Will Suratt <wsuratt@comcast.net>
2024-01-04 18:19:52 -0500

    icon merge

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,30 +19,35 @@ const Navbar = ({ token, handleLogout }: NavbarProps) => {
   }
   
   return (
-    <div className="navbar">
-      <Link to={CLIENT_BASE}><h1 className='nav-logo'><FontAwesomeIcon icon={faMugSaucer} />WORK3</h1></Link>
-      <div className="nav-links">
-        <Link className="no-style" to={CLIENT_BASE}><h4 className='nav-link'>Home</h4></Link>
-        <Link className="no-style" to="/tasks"><h4 className='nav-link'>Listings</h4></Link>
-        <Link className="no-style" to={CLIENT_BASE}><h4 className='nav-link'>Features</h4></Link>
-        <Link className="no-style" to="/about"><h4 className='nav-link'>About</h4></Link>
-      </div>
-      <div className="nav-btn-lgn">
-        {token ? (
-          <>
-            <Link to="/profile" className='nav-link nav-login no-style'>Profile</Link>
-            <a href={CLIENT_BASE} onClick={handleLogout} className='nav-link nav-login no-style logout'>Log out</a>
-          </>
-        ) : (
-          <div className="nav-btn-lgn">
-            <Link to="/login" className='nav-link nav-login no-style'>Log in</Link>
-            <Link to="/sign-up" className="no-style">
-              <div className="nav-exclamation">
-                Join rrivef for free
-              </div>
-            </Link>
+    <>
+      <div className="navbar">
+        <Link to={CLIENT_BASE}><h1 className='nav-logo'><FontAwesomeIcon className="nav-title-icon" icon={faMugSaucer} />WORK3</h1></Link>
+        <div className="nav-links">
+          <Link className="no-style" to={CLIENT_BASE}><h4 className='nav-link home-link'>Home</h4></Link>
+          <Link className="no-style" to="/tasks"><h4 className='nav-link'>Listings</h4></Link>
+          <Link className="no-style" to={CLIENT_BASE}><h4 className='nav-link'>Features</h4></Link>
+          <Link className="no-style" to="/about"><h4 className='nav-link'>About</h4></Link>
+        </div>
+        <div className="nav-btn-lgn">
+          {token ? (
+            <>
+              <Link to="/profile" className='nav-link nav-login no-style'>Profile</Link>
+              <a href={CLIENT_BASE} onClick={handleLogout} className='nav-link nav-login no-style logout'>Log out</a>
+            </>
+          ) : (
+            <div className="nav-btn-lgn">
+              <Link to="/login" className='nav-link nav-login no-style'>Log in</Link>
+              <Link to="/sign-up" className="no-style">
+                <div className="nav-exclamation">
+                  Join rrivef for free
+                </div>
+              </Link>
+            </div>
+          )}
+          <div className="hamburger-button" onClick={switchPopup}>
+            <FontAwesomeIcon icon={faBars} />
           </div>
-        )}
+        </div>
       </div>
       <div className="spacer" />
       {popupActive ?  
