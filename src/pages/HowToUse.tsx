9cdb2683d9c6c3ec027937def3163120d03a3c72
nathan-lesny <nlesny15@gmail.com>
2024-01-22 22:03:12 -0500

    create files

---
+++ src/pages/HowToUse.tsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar';
+
+export default function HowToUse() {
+  return (
+    <>
+      <Navbar />
+      <div className="about-container">
+        <h1>How to use Work3:</h1>
+        <div className="poster-container">
+          <h1>To post a task:</h1>
+        </div>
+      </div>
+    </>
+  )
+}
